91c758bbb95c745f760e80846e9728518a1805d4
Jose Acosta <jjosealf94@gmail.com>
2015-06-18 04:34:40 -0500

    Carga tarea 3

--- python/exercises/pyworkbook-50.py
+++ python/exercises/pyworkbook-50.py
@@ -2,27 +2,27 @@
 #: Description : This script gives the solution to the exercise 50 of the python woorkbook
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from math import sqrt
 
 print ("Exercise 50: Roots of cuadratic function\n")
 print ("Program that computes the real roots of a quadratic function\n")
 print ("Enter the coeficient of the quadratic equation:")
-a=float(raw_input("a="))
-b=float(raw_input("b="))
-c=float(raw_input("c="))
+a=float(input("a="))
+b=float(input("b="))
+c=float(input("c="))
 discriminant= b**2 - 4*a*c
 if discriminant > 0:
-    print "The real roots are:"
-    print "root=",( -1*b + sqrt(discriminant) )/2*a
-    print "root=",( -1*b - sqrt(discriminant) )/2*a
+    print ("The real roots are:")
+    print ("root=",( -1*b + sqrt(discriminant) )/2*a)
+    print ("root=",( -1*b - sqrt(discriminant) )/2*a)
 elif discriminant==0:
-    print "The double real root is:"
-    print "root=",(-1*b/2*a)
+    print ("The double real root is:")
+    print ("root=",(-1*b/2*a))
 else:
-    print "no real roots"
+    print ("no real roots")
 
-x=np.linspace(-5,5,0.1)
+x=np.linspace(-5,5,100)
 y= a*(x**2)+ b*(x) + c
 plt.plot(x,y)
 plt.show()
